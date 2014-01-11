1bc52a37c530f5e14cee4339680bf95084ce61a6
ryonext <ryonext.s@gmail.com>
2014-01-11 20:26:11 +0900

    extract first and second column.

--- lib/CosVON.rb
+++ lib/CosVON.rb
@@ -7,7 +7,9 @@ module Cosvon
   class << self
     def parse(cosvon)
       raise InvalidDataError unless cosvon.split("\n").first =~ /CoSVON:0\.1/i 
-      Hash[CSV.parse(cosvon)[1..-1]]
+      Hash[CSV.parse(cosvon)[1..-1].map do |arr|
+        arr.first(2) if arr[0] && arr[1]
+      end.compact]
     end
   end
 end
--- spec/lib/cosvon_spec.rb
+++ spec/lib/cosvon_spec.rb
@@ -2,19 +2,77 @@ require "spec_helper"
 
 describe Cosvon do
   describe ".parse" do
-    it "return hash from Cosvon" do
-      result = described_class.parse(<<-COSVON)
+    [ [ <<-COSVON, 
 CoSVON:0.1
 DreamCast,SEGA
 HI-Saturn,Hitachi
 FamiconTV,Sharp
-                            COSVON
+          COSVON
+         {
+            "DreamCast"=>"SEGA",
+            "HI-Saturn"=>"Hitachi",
+            "FamiconTV"=>"Sharp",
+         }
+      ],
+      [ <<-COSVON,
+CoSVON:0.1
+foo,bar
+           COSVON
+         {
+           "foo"=>"bar",
+         }
+      ],
+      [ <<-COSVON,
+CoSVON:0.1
+comma,"[,]"
+dq,"[""]"
+n,"[
+]"
+tab,[	]
+           COSVON
+        {
+          "comma"=>"[,]",
+          "dq"=>'["]',
+          "n"=>"[\n]",
+          "tab"=>"[\t]",
+        }
+      ],
+      [ <<-COSVON,
+CoSVON:0.1
+SH3,Super Hitachi 3
+ATOK,Awa TOKushima
+ICU,Isolated Crazy Utopia
+BING,Bing Is Not Google
+           COSVON
+           {
+             "SH3"=>"Super Hitachi 3",
+             "ATOK"=>"Awa TOKushima",
+             "ICU" => "Isolated Crazy Utopia",
+             "BING"=>"Bing Is Not Google",
+           }
+      ],
+      [ <<-COSVON,
+CoSVON:0.1,,,
+Google,Chrome,,
+Apple,Safari,"",
+Opera,Opera,,""
+Mozilla,Firefox,"",""
+,,,
+,,,
+,,,
+           COSVON
+           {
+             "Google"=>"Chrome",
+             "Apple"=>"Safari",
+             "Opera"=>"Opera",
+             "Mozilla"=>"Firefox"
+           }
+      ],
+    ].each do |cosvon, expected|
 
-      expect(result).to eq(
-        "DreamCast"=>"SEGA",
-        "HI-Saturn"=>"Hitachi",
-        "FamiconTV"=>"Sharp",
-      )
+      it "return hash from Cosvon" do
+        expect(described_class.parse(cosvon)).to eq(expected)
+      end
     end
     context "Invalid data" do
       context "which does'nt start with cosvon type" do
