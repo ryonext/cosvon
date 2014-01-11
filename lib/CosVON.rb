40d580a7d8b16356281a5040a601d03b623b8f5b
ryonext <ryonext.s@gmail.com>
2014-01-11 19:49:25 +0900

    Add first test.

--- lib/CosVON.rb
+++ lib/CosVON.rb
@@ -1,5 +1,11 @@
-require "Cosvon/version"
+require "cosvon/version"
+require "csv"
 
 module Cosvon
   # Your code goes here...
+  class << self
+    def parse(cosvon)
+
+    end
+  end
 end
--- spec/lib/cosvon_spec.rb
+++ spec/lib/cosvon_spec.rb
@@ -2,4 +2,21 @@ require "spec_helper"
 
 describe Cosvon do
   it { true }
+  describe ".parse" do
+    it "return hash from Cosvon" do
+      result = described_class.parse(<<-COSVON)
+                            CoSVON:0.1
+                            DreamCast,SEGA
+                            HI-Saturn,Hitachi
+                            FamiconTV,Sharp
+                            COSVON
+
+      expect(result).to eq(
+        "DreamCast"=>"SEGA",
+        "HI-Saturn"=>"Hitachi",
+        "FamiconTV"=>"Sharp",
+      )
+      
+    end
+  end
 end
